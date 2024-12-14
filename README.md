40e0169dc6f476349edba9dcb8c2f14bc5cd97d4
WitheringAway <124115470+notwithering@users.noreply.github.com>
2024-12-14 15:36:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# shit - **sh**are **it**
+
+simple and effective fileserver written in go to share files quickly
+
+shit when i first created it was just a go script that ran an http.FileServer on the current directory but i kept adding more features as i needed them over time (hosting a single file, hosting multiple files, hosting both files and directories, etc.) but now i think its ready for other people to use
+
+shit is a successor to my previous project [vmshare](https://github.com/notwithering/vmshare)
+
+```
+go install github.com/notwithering/shit@latest
+```
+
+## features
+
+- file and directory sharing
+	+ allows serving multiple files or directories specified via command-line arguments
+	+ defaults to serving the current working directory if no arguments are provided
+- port specification
+	+ customizable server port using `--port` or `-p` flag
+	+ defaults to port 8080 if not specified
+- dynamic file and directory browsing
+	+ automatically lists directory contents as clickable links in an html interface
+	+ redirects to a file or directory if the root directory contains a single export target
+- file handling
+	+ serves files with their correct MIME types using [`github.com/h2non/filetype`](https://github.com/h2non/filetype) library
+	+ reads and streams file content directly
+- error handling
+	+ provides error logging in the console including timestamps for debugging
+	+ returns appropriate http error response to the client for issues like missing files or directories
+- path validation and security
+	+ ensured safe file and directory resolution to prevent directory traversal attacks
+- ease of use
+	+ supports simple command line usage with the [`github.com/alecthomas/kingpin`](https://github.com/alecthomas/kingpin) library for argument parsing
+	+ automatically displays the local server address (`http://127.0.0.1:<port>`)
+
+## examples
+
+<!-- code examples labeled as C++ for better bash formatting -->
+
+```cpp
+$ echo "hello" > a.txt
+$ echo "hi" > b.txt
+$ mkdir dir
+$ echo "hey" > dir/c.txt
+```
+
+host current directory
+
+```cpp
+$ shit
+$ curl 127.0.0.1:8080
+a.txt
+b.txt
+dir/
+$ curl 127.0.0.1:8080/a.txt
+hello
+```
+
+host a single file
+
+```cpp
+$ shit b.txt
+$ curl -L 127.0.0.1:8080
+hi
+```
+
+host a few files
+
+```cpp
+$ shit a.txt b.txt
+$ curl 127.0.0.1:8080
+a.txt
+b.txt
+```
+
+host a file and a directory
+
+```cpp
+$ shit a.txt dir/
+$ curl 127.0.0.1:8080
+a.txt
+dir/
+$ curl 127.0.0.1:8080/dir
+c.txt
+$ curl 127.0.0.1:8080/dir/c.txt
+hey
+```
+
+etc.
+
+## licenses
+
+this project uses the following dependencies with the license as noted:
+
+- [github.com/alecthomas/kingpin](https://github.com/alecthomas/kingpin) - MIT License
+- [github.com/h2non/filetype](https://github.com/h2non/filetype) - MIT License
+
+each dependency retains its respective license. for more details refer to their official documentation or source code
---
+++ main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"io"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/h2non/filetype"
+)
+
+const (
+	defaultPort = "8080"
+)
+
+var (
+	tmpl = template.Must(template.New("").Parse(tmplStr))
+
+	portFlag   = kingpin.Flag("port", "The port to serve.").Short('p').Default("8080").String()
+	port       string
+	exportsArg = kingpin.Arg("files", "The files or directories to share.").Default(".").ExistingFilesOrDirs()
+	exports    []string
+)
+
+func main() {
+	kingpin.Parse()
+	port = *portFlag
+	exports = *exportsArg
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			if err := serveRoot(w, r); err != nil {
+				httpErr(w, err)
+			}
+
+			return
+		}
+
+		if err := serveExports(w, r); err != nil {
+			httpErr(w, err)
+		}
+	})
+
+	fmt.Printf("http://127.0.0.1:%s/\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func serveRoot(w http.ResponseWriter, r *http.Request) error {
+	if len(exports) == 1 {
+		export := exports[0]
+
+		fileinfo, err := os.Stat(export)
+		if err != nil {
+			return err
+		}
+
+		if fileinfo.IsDir() {
+			dir, err := os.ReadDir(export)
+			if err != nil {
+				return err
+			}
+
+			return executeDirEntries(w, dir)
+		} else {
+			http.Redirect(w, r, filepath.Base(export), http.StatusPermanentRedirect)
+			return nil
+		}
+	}
+
+	return executeExports(w)
+}
+
+func serveExports(w http.ResponseWriter, r *http.Request) error {
+	requestedFile := filepath.Clean(r.URL.Path[1:])
+	var filePath string
+
+	for _, export := range exports {
+		fileinfo, err := os.Stat(export)
+		if err != nil {
+			fmt.Println("nil")
+			return err
+		}
+
+		if !fileinfo.IsDir() {
+			export = "."
+		}
+
+		file := filepath.Join(export, requestedFile)
+
+		fileinfo, err = os.Stat(file)
+		if errors.Is(err, os.ErrNotExist) {
+			continue
+		} else if err != nil {
+			fmt.Println("nil")
+			return err
+		}
+
+		exportAbs, err := filepath.Abs(export)
+		if err != nil {
+			return err
+		}
+
+		fileAbs, err := filepath.Abs(export)
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(exportAbs, fileAbs)
+		if err != nil {
+			return err
+		}
+
+		if !strings.HasPrefix(relPath, "..") {
+			filePath = file
+			break
+		}
+	}
+
+	if filePath == "" {
+		http.NotFound(w, r)
+		return nil
+	}
+
+	fileinfo, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+
+	if fileinfo.IsDir() {
+		if !strings.HasSuffix(r.URL.Path, "/") {
+			http.Redirect(w, r, r.URL.Path+"/", http.StatusPermanentRedirect)
+			return nil
+		}
+
+		dir, err := os.ReadDir(filePath)
+		if err != nil {
+			return err
+		}
+
+		return executeDirEntries(w, dir)
+	}
+
+	return serveFile(w, filePath)
+}
+
+func executeDirEntries(w http.ResponseWriter, dir []fs.DirEntry) error {
+	var links []string
+
+	for _, file := range dir {
+		name := file.Name()
+		if file.IsDir() {
+			name += "/"
+		}
+		links = append(links, name)
+	}
+
+	return tmpl.Execute(w, links)
+}
+
+func executeExports(w http.ResponseWriter) error {
+	var links []string
+
+	for _, file := range exports {
+		fileinfo, err := os.Stat(file)
+		if err != nil {
+			return err
+		}
+
+		name := filepath.Base(file)
+		if fileinfo.IsDir() {
+			name += "/"
+		}
+		links = append(links, filepath.Base(file))
+	}
+
+	return tmpl.Execute(w, links)
+}
+
+func httpErr(w http.ResponseWriter, err error) {
+	fmt.Printf("[%s] %s\n", time.Now().Format(time.DateTime), err.Error())
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+func serveFile(w http.ResponseWriter, filename string) error {
+	file, err := os.OpenFile(filename, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	buf, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	mtype, err := filetype.Match(buf)
+	w.Header().Set("Content-Type", mtype.MIME.Type)
+
+	_, err = w.Write(buf)
+	return err
+}
+
+const tmplStr string = `<!DOCTYPE html>
+<html data-fbscriptallow="true"><head><meta name="viewport" content="width=device-width"></head><body>
+<pre>{{range .}}<a href="{{.}}">{{.}}</a>
+{{end}}</pre>
+</body></html>`
